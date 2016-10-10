5a753ad77407c1abcbfec9add9e70c64f8446069
Иванов Анатолий <Anatoly.Ivanov@centre-it.com>
2016-10-10 10:17:06 +0500

    extern layers configure

--- leaflet.js
+++ leaflet.js
@@ -15,14 +15,18 @@
         transclude: true,
         replace: true,
         scope: {
-            server: '=server',
             control: '=',
             items: '=',
             lat: '@',
             lon: '@',
-            name: '@'
+            baseLat: '@',
+            baseLon: '@',
+            baseScale: '@',
+            name: '@',
+            subname: '@',
+            layers: '='
         },
-        template: 'template/leaflet.html',
+        templateUrl: 'template/leaflet.html',
         controller: ["$scope", function(scope) {
             // Позволяет использовать путь до нужного свойства
             scope.getDescendantProp = function(obj, desc) {
@@ -32,7 +36,7 @@
             }
         }],
         link: function (scope, element, attr) {
-            scope.mymap = L.map(element[0]).setView([55.15, 61.40], 13);
+            scope.mymap = L.map(element[0]).setView([scope.baseLat, scope.baseLon], scope.baseScale);
 
             scope.markers = [];
 
@@ -41,10 +45,22 @@
                 scope.mymap.invalidateSize();
             }
 
+            if (scope.layers && scope.layers.wms) {
+                angular.forEach(scope.layers.wms, function(value, key) {
+                    L.tileLayer.wms(value.url, {
+                        layers: value.layers,
+                        crc: value.crc,
+                        transparent: value.transparent,
+                        format: value.format
+                    }).addTo(scope.mymap);
+                });
+            }
+
+            /*
             L.tileLayer(scope.server, {
                 maxZoom: 18
             }).addTo(scope.mymap);
-
+            */
             $(element[0].firstChild).resize(function() {
                 scope.mymap.invalidateSize();
             })
@@ -55,14 +71,18 @@
                 }
                 scope.markers = [];
                 for (var i = 0; i < scope.items.length; i++) {
-                    var icon = L.ExtraMarkers.icon({
-                        icon: 'fa-car',
-                        markerColor: 'red',
-                        shape: 'square',
-                        prefix: 'fa'
+                    var icon = L.icon({
+                        iconUrl: 'img/markers/customs.png',
+
+                        iconSize:     [32, 37], // size of the icon
+                        shadowSize:   [50, 64], // size of the shadow
+                        iconAnchor:   [16, 37], // point of the icon which will correspond to marker's location
+                        shadowAnchor: [16, 37],  // the same for the shadow
+                        popupAnchor:  [16, -37] // point from which the popup should open relative to the iconAnchor
                     });
-                    scope.markers[i] = new L.marker([scope.items[i][scope.lat], scope.items[i][scope.lon]], {icon: icon});
-                    scope.markers[i].bindLabel(scope.getDescendantProp(scope.items[i], scope.name), {noHide: true, className: "car-label", offset: [0, 0] });
+                    scope.markers[i] = new L.marker([scope.getDescendantProp(scope.items[i], scope.lat), scope.getDescendantProp(scope.items[i], scope.lon)], {icon: icon});
+                    scope.markers[i].bindPopup('<b>' + scope.getDescendantProp(scope.items[i], scope.name) + '</b></br><small class="text-muted">' + scope.getDescendantProp(scope.items[i], scope.subname) + '</small><hr><i class="fa fa-male"></i><i class="fa fa-train"></i>').openPopup();
+                    /*scope.markers[i].bindLabel(scope.getDescendantProp(scope.items[i], scope.name), {noHide: true, className: "car-label", offset: [0, 0] });*/
                     scope.markers[i].addTo(scope.mymap);
                 }
             }
