bb15768bdb70fd74035ca4946a33c6b5c5f50af6
Иванов Анатолий <Anatoly.Ivanov@centre-it.com>
2016-10-27 14:34:50 +0500

    add icon control

--- leaflet.js
+++ leaflet.js
@@ -15,16 +15,30 @@
         transclude: true,
         replace: true,
         scope: {
-            control: '=',
+            control: '=?',
             items: '=',
-            lat: '@',
-            lon: '@',
-            baseLat: '@',
-            baseLon: '@',
-            baseScale: '@',
+            /* Путь до свойства с широтой */
+            lat: '@?',
+            /* Путь до свойства с долготой */
+            lon: '@?',
+            /* Путь до свойства, по которому можно дифференцировать иконку */
+            iconDiffPath: '@?',
+            /* Функция, получающая код (название) части иконки по объекту diff */
+            iconDiffFunc: '=?',
+            /* Путь до свойства, по которому можно статус объекта (для определения иконки) */
+            iconStatusPath: '@?',
+            /* Функция, получающая код (название) части иконки по объекту status */
+            iconStatusFunc: '=?',
+            /* Базовый шаблон для определения адреса иконки (передаются ппараметры diff и статус) */
+            iconBaseFormat: '@?',
+            /* Настройки размера иконок */
+            iconSettings: '=?',
+            baseLat: '@?',
+            baseLon: '@?',
+            baseScale: '@?',
             name: '@',
             subname: '@',
-            selectCallback: '=selectOnMapCallback',
+            selectCallback: '=?selectOnMapCallback',
             layers: '='
         },
         templateUrl: 'template/leaflet.html',
@@ -37,7 +51,7 @@
             }
         }],
         link: function (scope, element, attr) {
-            scope.mymap = L.map(element[0]).setView([scope.baseLat, scope.baseLon], scope.baseScale);
+            scope.mymap = L.map(element[0]).setView([scope.baseLat ? scope.baseLat : 0, scope.baseLon ? scope.baseLon : 0], scope.baseScale ? scope.baseScale : 13);
 
             scope.markers = [];
 
@@ -79,23 +93,46 @@
                 }
                 scope.markers = [];
                 for (var i = 0; i < scope.items.length; i++) {
-                    var icon = L.icon({
-                        iconUrl: 'img/markers/customs.png',
 
-                        iconSize:     [32, 37], // size of the icon
-                        shadowSize:   [50, 64], // size of the shadow
-                        iconAnchor:   [16, 37], // point of the icon which will correspond to marker's location
-                        shadowAnchor: [16, 37],  // the same for the shadow
-                        popupAnchor:  [16, -37] // point from which the popup should open relative to the iconAnchor
-                    });
-                    scope.markers[i] = new L.marker([scope.getDescendantProp(scope.items[i], scope.lat), scope.getDescendantProp(scope.items[i], scope.lon)], {icon: icon});
-                    scope.markers[i].bindPopup('<b>' + scope.getDescendantProp(scope.items[i], scope.name) + '</b></br><small class="text-muted">' + scope.getDescendantProp(scope.items[i], scope.subname) + '</small><hr><i class="fa fa-male"></i><i class="fa fa-train"></i>').openPopup();
-                    /*scope.markers[i].bindLabel(scope.getDescendantProp(scope.items[i, scope.name), {noHide: true, className: "car-label", offset: [0, 0] });*/
-                    scope.markers[i].addTo(scope.mymap).on('click', function(item) {
-                        return function() {
-                            scope.selectCallback(item)
-                        };
-                    }(scope.items[i]));
+                    /* Определение иконки по главному свойству */
+                    var iconValueRaw = scope.getDescendantProp(scope.items[i], scope.iconDiffPath);
+                    if (iconValueRaw && scope.iconDiffFunc) {
+                        var iconValue = scope.iconDiffFunc(iconValueRaw);
+                    } else {
+                        var iconValue = iconValueRaw;
+                    }
+
+                    /* Определение иконки по втричному свойству (статусу) */
+                    var iconStatusRaw = scope.getDescendantProp(scope.items[i], scope.iconStatusPath);
+                    if (iconStatusRaw && scope.iconStatusFunc) {
+                        var iconStatus = scope.iconStatusFunc(iconStatusRaw);
+                    } else {
+                        var iconStatus = iconStatusRaw;
+                    }
+
+                    if (iconValue) {
+                        /* Формирвоание конечного URL иконки по главному и вторичному признаку */
+                        var iconURL = scope.format(scope.iconBaseFormat, iconValue, iconStatus);
+                        var icon = L.icon({
+                            iconUrl: iconURL,
+
+                            iconSize: (scope.iconSettings) ? (scope.iconSettings.iconSize) : undefined,
+                            shadowSize: (scope.iconSettings) ? (scope.iconSettings.shadowSize) : undefined,
+                            iconAnchor: (scope.iconSettings) ? (scope.iconSettings.iconAnchor) : undefined,
+                            shadowAnchor: (scope.iconSettings) ? (scope.iconSettings.shadowAnchor) : undefined,
+                            popupAnchor: (scope.iconSettings) ? (scope.iconSettings.popupAnchor) : undefined
+                        });
+                        scope.markers[i] = new L.marker([scope.getDescendantProp(scope.items[i], scope.lat), scope.getDescendantProp(scope.items[i], scope.lon)], {icon: icon});
+                        scope.markers[i].bindPopup('<b>' + scope.getDescendantProp(scope.items[i], scope.name) + '</b></br><small class="text-muted">' + scope.getDescendantProp(scope.items[i], scope.subname) + '</small><hr><i class="fa fa-male"></i><i class="fa fa-train"></i>').openPopup();
+                        /*scope.markers[i].bindLabel(scope.getDescendantProp(scope.items[i, scope.name), {noHide: true, className: "car-label", offset: [0, 0] });*/
+                        scope.markers[i].addTo(scope.mymap).on('click', function (item) {
+                            return function () {
+                                if (scope.selectCallback) {
+                                    scope.selectCallback(item)
+                                }
+                            };
+                        }(scope.items[i]));
+                    }
                 }
             }
 
@@ -122,6 +159,13 @@
                 function () {
                     scope.mymap.invalidateSize();
                 });
+
+            scope.format = function(str) {
+                var args = arguments;
+                return str.replace(/{[0-9]}/g, function(matched) {
+                    return args[parseInt(matched.replace(/[{}]/g, '')) + 1];
+                });
+            }
         }
     };
 });
